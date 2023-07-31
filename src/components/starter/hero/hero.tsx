e0df54b13300e185320766159039d08fcb63ef89
Raihan Nismara <raihannismara@gmail.com>
2023-07-31 19:52:16 +0700

    Merubah text fantastic menjadi welcome

--- src/components/starter/hero/hero.tsx
+++ src/components/starter/hero/hero.tsx
@@ -7,9 +7,7 @@ export default component$(() => {
     <div class={["container", styles.hero]}>
       <ImgThunder class={styles["hero-image"]} />
       <h1>
-        So <span class="highlight">fantastic</span>
-        <br />
-        to have <span class="highlight">you</span> here
+       Welcome
       </h1>
       <p>Have fun building your App with Qwik.</p>
       <div class={styles["button-group"]}>
